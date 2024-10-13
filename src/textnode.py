5e757fb37ffef38926d271417e73e338eea8ed4d
Damien Nelson <dhnisaman@gmail.com>
2024-10-12 23:36:38 -0400

    set up textnode class add eq and repr methods

---
+++ src/textnode.py
@@ -0,0 +1,17 @@
+class TextNode():
+  def __init__(self, text, text_type, url):
+    self.text = text
+    self.text_type = text_type
+    self.url = url
+
+  def __eq__(self, node1):
+    return (
+      isinstance(node1, TextNode) and
+      self.text == node1.text and
+      self.text_type == node1.text_type and
+      self.url == node1.url
+    )
+
+  def __repr__(self):
+    return f"TextNode({self.text}, {self.text_type}, {self.url})"
+  
