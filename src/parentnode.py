9183ac19362a474d7e71a2ca4a5e38815a024546
Damien Nelson <dhnisaman@gmail.com>
2024-10-15 22:15:58 -0400

    add parentnode class add recursive to html method

---
+++ src/parentnode.py
@@ -0,0 +1,16 @@
+from htmlnode import HTMLNode
+
+class ParentNode(HTMLNode):
+  def __init__(self, tag=None, children=None, props=None):
+    super().__init__(tag, None, children, props)
+    if self.children == None or not self.children:
+      raise ValueError("parent node must have children")
+    if self.tag == None:
+      raise ValueError("parent node must have a tag")
+    
+  def to_html(self):
+    result = ""
+    for child in self.children:
+      result += child.to_html()
+    return f"""<{self.tag}{self.props_to_html()}>{result}</{self.tag}>"""
+      
