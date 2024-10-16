87ec37a9fd97e124943215f07da552a542c172cd
Damien Nelson <dhnisaman@gmail.com>
2024-10-15 22:16:18 -0400

    add test for leafnode

---
+++ src/test_leafnode.py
@@ -0,0 +1,25 @@
+import unittest
+
+import unittest.test
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+  def test_create_node_raise(self):
+    with self.assertRaises(ValueError):
+      LeafNode()
+    with self.assertRaises(ValueError):
+      LeafNode("b")
+
+  def test_create_node_pass(self):
+    node = LeafNode("a", "click", {"href": "https://www.google.com"})
+    node2 = LeafNode(None,"this is a test", None)
+    self.assertIsInstance(node, LeafNode)
+    self.assertIsInstance(node2, LeafNode)
+
+  def test_to_html(self):
+    node = LeafNode("a", "click", {"href": "https://www.google.com"})
+    node2 = LeafNode(None,"this is a test", None)
+    html_tag = node.to_html()
+    string = node2.to_html()
+    self.assertEqual(html_tag, '''<a href="https://www.google.com">click</a>''')
+    self.assertEqual(string, "this is a test")
